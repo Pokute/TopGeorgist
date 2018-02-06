63133920d4dbf46d84da32cf65d8c85e8d07e358
Eero Tanskanen <pokute@draconus.com>
2018-02-06 23:34:25 +0200

    Added Redux

---
+++ topGeorgistRedux.mjs
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const TopGeorgist = () => (
+	<p>Foobar</p>
+);
+
+export default TopGeorgist;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
 	entry: [
 		'@babel/polyfill',
-		'./topGeorgist'
+		// './topGeorgist'
+		'./reduxIndex'
 	],
 	output: {
 		filename: 'static/topGeorgist.bundle.js'
