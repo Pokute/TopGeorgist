f6831006b22f8a3c5057c319ce65a23b381867e4
Eero Tanskanen <pokute@draconus.com>
2019-07-18 23:41:45 +0300

    More goal debugging info in Inventory

--- ui/InventoryTgo.tsx
+++ ui/InventoryTgo.tsx
@@ -1,9 +1,69 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import { InventoryItem } from '../components/inventory';
-import { isComponentGoal } from '../data/components_new';
+import { InventoryItem, hasComponentInventory } from '../components/inventory';
+import { isComponentGoal, isComponentWork, ComponentGoalDoer, ComponentGoal } from '../data/components_new';
 import { RootStateType } from '../reducers';
+import InventoryReact from './inventory.react';
+import { TgoId } from '../reducers/tgo';
+import Category from './Category';
+
+const RenderWorkInstance = ({ workTgoId }: { workTgoId: TgoId }) => {
+	const wiTgo = useSelector((store: RootStateType) => store.tgos[workTgoId]);
+	if (isComponentWork(wiTgo)) {
+		return (
+			<div>
+				<div>Type: {wiTgo.work.type}</div>
+				<div>Target: {wiTgo.workTargetTgoId}</div>
+				<div>
+					ActorCommitted:
+					<InventoryReact
+						key={wiTgo.tgoId}
+						ownerTgoId={wiTgo.workActorCommittedItemsTgoId}
+					/>
+				</div>
+				<div>
+					TargetCommitted:
+					<InventoryReact
+						key={wiTgo.tgoId}
+						ownerTgoId={wiTgo.workTargetCommittedItemsTgoId}
+					/>
+				</div>
+			</div>
+		);
+	} else {
+		return (<span>
+			NoWorkInstanceInv!
+		</span>)
+	}
+};
+
+const RenderGoal = ({ tgo, tgoData }: { tgo: ComponentGoal, tgoData: string }) => (
+	<Category
+		title={`Goal: ${tgo.goal.title}`}
+	>
+		<span>{`${tgoData}`}</span>
+		{tgo.goal.requirements.map(req => {
+			switch(req.type) {
+				case 'RequirementMove': {
+					return (<span key={req.type}>
+						{`Req move to: ${req.targetPosition.x}, ${req.targetPosition.y}`}
+					</span>);
+				}
+				default:
+					return (<span key={req.type}>
+						{`unknown type`}
+					</span>);
+			}
+		})}
+		{tgo.goal.workInstances.map(wiTgoId => (
+			<RenderWorkInstance
+				key={wiTgoId}
+				workTgoId={wiTgoId}
+			/>	
+		))}
+	</div>
+);
 
 const InventoryTgo = ({ i }: { i: InventoryItem }) => {
 	if (i.typeId !== 'tgoId' || !i.tgoId) {
@@ -14,13 +74,17 @@ const InventoryTgo = ({ i }: { i: InventoryItem }) => {
 	if (!tgo) {
 		return (<span>Tgo not found for id ${i.tgoId}</span>);
 	}
+
+	const tgoData = `TgoId: ${i.tgoId}. Count: ${i.count}.`;
+
 	if (isComponentGoal(tgo)) {
-		return (
-			<span>{`TgoId: ${i.tgoId}. ${i.count}. Goal: ${tgo.goal.title}, req: ${tgo.goal.requirements} wis: ${tgo.goal.workInstances}`}</span>
-		);
+		return (<RenderGoal
+				tgo={tgo}
+				tgoData={tgoData}
+			/>)
 	}
 	return (
-		<span>{`TgoId: ${i.tgoId}. ${i.count}`}</span>
+		<span>${tgoData}</span>
 	);
 };
 
