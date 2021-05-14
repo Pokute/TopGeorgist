9d07ad71831489d852de5dfad2c841442e05ba46
Eero Tanskanen <pokute@draconus.com>
2021-05-15 00:10:23 +0300

    Fix testUtils

--- tests/testUtils.test.ts
+++ tests/testUtils.test.ts
@@ -4,7 +4,7 @@ import { createAction } from 'typesafe-actions';
 import { omitType } from '../testUtils.js';
 
 test('omitType works properly', t => {
-	const action = createAction('OMITTYPE_TEST')();
+	const action = createAction('OMITTYPE_TEST', () => ({}))();
 
 	t.deepEqual(omitType(action()), { payload: {
 	} });
